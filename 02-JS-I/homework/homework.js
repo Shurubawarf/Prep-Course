f537a08d2f430e0f9736159dd6525c5a911e10a1
alan <obholzalan@gmail.com>
2022-05-11 19:06:30 -0300

    23 pasados

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,120 +28,136 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código: 
-  return ( 'str' )
+  return str 
   }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código: 
-  suma ( 1 + 2 )
+   var suma = x + y;
+   return suma;
   }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código: 
-  resta (5 - 7)
+   var resta = x - y;
+   return resta;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código: 
-  multiplica ( 4 * 10 )
-  
+  var multiplica  = 4 * 10; 
+  return multiplica;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código: 
-  divide (8 / 2 )
-  
+ var divide = x / y;
+  return divide;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código: 
-  sonIguales ( 6 = 6)
-}
-
-function tienenMismaLongitud(str1, str2) {
+   if ( x === y ) {
+       return true; 
+  }else { 
+        return false;}
+  } 
+ 
+  function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código: 
-  tienenMismaLongitud ( 'hola', 'hola')
-  
+  return (str1.length === str2.length);
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ( num < 90 ) {
+    return true; 
+  }else {
+    return false; }
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ( num > 50 ) {
+    return true; 
+    } else {
+      return false; }
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+ return x % y;
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+   if ( num % 2 === 0) {
+     return true; 
+   } else {
+     return false; }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if ( num % 2 === 1) {
+  return true;
+  }else {
+    return false; }
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+return num * num;
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return Math.pow (num, 3);
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  return Math.pow (num, exponent);
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  return Math.round (num);
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  return Math.ceil (num);
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return Math.random (1, 9);
 }
 
 function esPositivo(numero) {
