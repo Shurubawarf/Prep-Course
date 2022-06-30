7b09ff3620f2235a7a06388faebae4788197baef
alan <obholzalan@gmail.com>
2022-06-30 18:59:07 -0300

    js-04-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -10,13 +10,14 @@ return array [0];
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-
+return array [array.length -1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+ return array.length;
 }
 
 
@@ -25,6 +26,11 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+var nuevoarray = [];
+  for (var i = 0; i < array.length; i++){
+  nuevoarray[i] = array[i] + 1;
+}
+return nuevoarray;
 }
 
 
@@ -32,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+array [array.length] = elemento;
+return array;
 }
 
 
@@ -40,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+array.unshift (elemento);
+return array;
 }
 
 
@@ -49,27 +59,38 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+ return palabras.join ("  ");
 }
+
+
 
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (var i = 0; i < array.length; i++ ) {
+if (array [i] === elemento){ 
+return true;  
 }
-
-
+} 
+return false;
+}
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+var suma = [];
+for (var i = 0 ; i < numeros.length; i++ ){
+suma = suma + numeros [1];
 }
-
-
+return suma; 
+} 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest) / resultadosTest.length;
 }
 
 
@@ -77,37 +98,61 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+var maximo = numeros [0];
+for (var i = 0; i < numeros.length; i++) {
+   if (numeros[i] > maximo) {
+    mximo = numeros [i];
+   }
+  }
+  return maximo;
 }
 
 
-function multiplicarArgumentos() {
+
+function multiplicarArgumentos() { 
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length < 1) return 0;
+  var total = 1;
+  for(var i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
 }
-
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+let nuevocontador = [0];
+for (let i = 0; i < arreglo.length; i++) {
+  if (arreglo[i] > 18) {
+nuevocontador++;
+  }
 }
-
+return nuevocontador;
+}
 
 function diaDeLaSemana(numeroDeDia) {
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
-} 
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return "es fin de semana";
+  }
+return "Es dia laboral";
+}
 
 
 function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  if (n = 9) {
+    return true;
+  }
+return false;
 }
 
 
